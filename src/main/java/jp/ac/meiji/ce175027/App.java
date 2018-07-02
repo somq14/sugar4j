7448baa9988aec7bc744cb9c078c1114a2ffa54c
Kyu Matsuura <q.matsuura@gmail.com>
2018-07-02 21:09:59 +0900

    最初のコミット

---
+++ src/main/java/jp/ac/meiji/ce175027/App.java
@@ -0,0 +1,13 @@
+package jp.ac.meiji.ce175027;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
