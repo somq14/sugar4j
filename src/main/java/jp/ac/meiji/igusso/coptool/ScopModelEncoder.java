c441daa00d52756effe6cd69c47ec4bf92787033
Kyu Matsuura <q.matsuura@gmail.com>
2018-07-04 21:34:43 +0900

    ScopModelEncoderクラスの実装

---
+++ src/main/java/jp/ac/meiji/igusso/coptool/ScopModelEncoder.java
@@ -0,0 +1,94 @@
+package jp.ac.meiji.igusso.coptool;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public final class ScopModelEncoder implements ModelEncoder {
+  private static final ScopModelEncoder singleton = new ScopModelEncoder();
+
+  public static ScopModelEncoder getInstance() {
+    return singleton;
+  }
+
+  private ScopModelEncoder() {}
+
+  @Override
+  public List<String> encode(Variable variable) {
+    StringBuilder domainExp = new StringBuilder();
+    for (Integer d : variable.getDomain()) {
+      domainExp.append(d).append(", ");
+    }
+    domainExp.setLength(domainExp.length() - 2);
+
+    String body = String.format("variable %s in {%s}", variable.getName(), domainExp.toString());
+    return Arrays.asList(body);
+  }
+
+  @Override
+  public List<String> encode(LinearConstraint constraint) {
+    String weightExp = constraint.getWeight() < 0 ? "inf" : constraint.getWeight() + "";
+
+    StringBuilder termsBuilder = new StringBuilder();
+    for (int i = 0; i < constraint.size(); i++) {
+      int coeff = constraint.getCoeffs().get(i);
+      Variable var = constraint.getVariables().get(i);
+      int val = constraint.getValues().get(i);
+
+      termsBuilder.append(String.format("%d(%s, %d)", coeff, var.getName(), val));
+      termsBuilder.append(' ');
+    }
+    String termsExp = termsBuilder.toString();
+
+    String cons = null;
+
+    switch (constraint.getOp()) {
+      case EQ:
+        cons = String.format("%s: weight=%s type=linear %s %s %d", constraint.getName(), weightExp,
+            termsExp, "=", constraint.getRhs());
+        break;
+
+      case LE:
+        cons = String.format("%s: weight=%s type=linear %s %s %d", constraint.getName(), weightExp,
+            termsExp, "<=", constraint.getRhs());
+        break;
+
+      case LT:
+        cons = String.format("%s: weight=%s type=linear %s %s %d", constraint.getName(), weightExp,
+            termsExp, "<=", constraint.getRhs() - 1);
+        break;
+
+      case GE:
+        cons = String.format("%s: weight=%s type=linear %s %s %d", constraint.getName(), weightExp,
+            termsExp, ">=", constraint.getRhs());
+        break;
+
+      case GT:
+        cons = String.format("%s: weight=%s type=linear %s %s %d", constraint.getName(), weightExp,
+            termsExp, ">=", constraint.getRhs() + 1);
+        break;
+
+      default:
+        throw new IllegalStateException();
+    }
+
+    return Arrays.asList(cons);
+  }
+
+  @Override
+  public List<String> encode(Model model) {
+    List<String> body = new ArrayList<>();
+
+    for (Variable var : model.getVariables()) {
+      body.addAll(encode(var));
+    }
+
+    for (Constraint cons : model.getConstraints()) {
+      body.addAll(cons.encode(this));
+    }
+    return Collections.unmodifiableList(body);
+  }
+}
---
+++ src/test/java/jp/ac/meiji/igusso/coptool/ScopModelEncoderTest.java
@@ -0,0 +1,38 @@
+package jp.ac.meiji.igusso.coptool;
+
+import org.junit.Test;
+
+import static java.util.Arrays.asList;
+import static jp.ac.meiji.igusso.coptool.Comparator.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+public class ScopModelEncoderTest {
+  @Test
+  public void test() {
+    Model model = new Model();
+
+    Variable v1 = model.addVariable("v1", asList(1, 2, 3));
+    Variable v2 = model.addVariable("v2", asList(1, 2, 3));
+    Variable v3 = model.addVariable("v3", asList(1, 2, 3));
+
+    LinearConstraint c1 = LinearConstraint.of("c1", LE, 3)
+                             .addTerm(1, v1, 1)
+                             .addTerm(2, v2, 2)
+                             .addTerm(3, v3, 3)
+                             .build();
+    model.addConstraint(c1);
+
+    LinearConstraint c2 = LinearConstraint.of("c2", GE, 3, 1)
+                             .addTerm(3, v1, 1)
+                             .addTerm(2, v2, 2)
+                             .addTerm(1, v3, 3)
+                             .build();
+    model.addConstraint(c2);
+
+
+    for (String line : ScopModelEncoder.getInstance().encode(model)) {
+      System.out.println(line);
+    }
+  }
+}
